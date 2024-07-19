4c61f4432035c1a272a6ccd128c34dfea0e75b57
ilhomjon Solijonov <ilhomjonsolijonov843@gmail.com>
2024-07-19 20:10:03 +0500

    Start of project.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar/index'
+
+function App() {
+
+  return (
+    <div className='app-box'>
+      <Navbar/>
+      <div className='app-content'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
