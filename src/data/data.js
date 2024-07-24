32c9624da232e803b4882933e0de7c5e0c860737
ilhomjon Solijonov <ilhomjonsolijonov843@gmail.com>
2024-07-24 13:32:45 +0500

    Portfolio page is completed.

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import Image1 from "../assets/image1.png"
+
+
+const Portfolio = [
+    {
+        id: 1, 
+        img: Image1,
+        name: "Medbrat",
+        category: "application",
+        link: 'hhtps://medbrat.netlify.app',
+    },
+    {
+        id: 2, 
+        img: Image1,
+        name: "Medbrat",
+        category: "application",
+        link: 'hhtps://medbrat.netlify.app',
+    },
+    {
+        id: 3, 
+        img: Image1,
+        name: "Medbrat",
+        category: "web",
+        link: 'hhtps://medbrat.netlify.app',
+    },
+    {
+        id: 4, 
+        img: Image1,
+        name: "design",
+        category: "design",
+        link: 'hhtps://medbrat.netlify.app',
+    }
+]
+
+export default Portfolio
--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
+import './style.css'
 import Menu from '../../components/Menu/index'
+import { NavLink } from 'react-router-dom'
+import Portfolio from '../../data/data'
+
+
 const index = () => {
+
+  
+const [filterDat,setFilterDat]=useState(Portfolio)
+
+ const FilterWeb = ()=> {
+    const FData3=Portfolio.filter(item=>item.category==='web')
+    setFilterDat(FData3)
+ }
+ const FilterDesign = ()=> {
+  const FData3=Portfolio.filter(item=>item.category==='design')
+  setFilterDat(FData3)
+}
+const FilterApp = ()=> {
+  const FData3=Portfolio.filter(item=>item.category==='application')
+  setFilterDat(FData3)
+}
+const FilterAll = ()=> {
+  setFilterDat(Portfolio)
+}
+
   return (
     <div className='about-box'>
       <header className='header'>
@@ -9,6 +34,31 @@ const index = () => {
           <Menu/>
         </div>
       </header>
+      <main className='about-content'>
+        <ul className='portfolio-menu'>
+          <li onClick={FilterAll}>
+            <NavLink>All</NavLink>
+          </li>
+          <li onClick={FilterWeb}>
+            <NavLink>Web Development</NavLink>
+          </li>
+          <li onClick={FilterApp}>
+            <NavLink>Application</NavLink>
+          </li>
+          <li onClick={FilterDesign}>
+            <NavLink>Web Design</NavLink>
+          </li>
+        </ul>
+        <section className='portfolio-content'>
+            {filterDat.map(item => (
+                <a href={item.link} className='portfolio-card' key={item.id}>
+                  <img src={item.img} alt="" />
+                  <h2>{item.name}</h2>
+                  <p>{item.category}</p>
+                </a>
+            ))}
+        </section>
+      </main>      
     </div>
   )
 }
